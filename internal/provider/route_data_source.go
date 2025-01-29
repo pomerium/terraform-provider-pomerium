5027f4d6edcacc5bf871ed88d80e70541fe6787e
Caleb Doxsey <cdoxsey@pomerium.com>
2025-01-29 12:02:08 -0700

    routes: update data source (#33)
    Update the routes data source, which was missing some fields. Also add
    the additional list parameters supported by the API.

--- internal/provider/route_data_source.go
+++ internal/provider/route_data_source.go
@@ -15,6 +15,159 @@ import (
 
 var _ datasource.DataSource = &RouteDataSource{}
 
+func getRouteDataSourceAttributes(idRequired bool) map[string]schema.Attribute {
+	return map[string]schema.Attribute{
+		"id": schema.StringAttribute{
+			Description: "Unique identifier for the route.",
+			Required:    idRequired,
+			Computed:    !idRequired,
+		},
+		"name": schema.StringAttribute{
+			Computed:    true,
+			Description: "Name of the route.",
+		},
+		"from": schema.StringAttribute{
+			Computed:    true,
+			Description: "From URL.",
+		},
+		"to": schema.ListAttribute{
+			Computed:    true,
+			ElementType: types.StringType,
+			Description: "To URLs.",
+		},
+		"namespace_id": schema.StringAttribute{
+			Computed:    true,
+			Description: "ID of the namespace the route belongs to.",
+		},
+		"policies": schema.ListAttribute{
+			Computed:    true,
+			ElementType: types.StringType,
+			Description: "List of policy IDs associated with the route.",
+		},
+		"stat_name": schema.StringAttribute{
+			Computed:    true,
+			Description: "Name of the stat.",
+		},
+		"prefix": schema.StringAttribute{
+			Computed:    true,
+			Description: "Prefix.",
+		},
+		"path": schema.StringAttribute{
+			Computed:    true,
+			Description: "Path.",
+		},
+		"regex": schema.StringAttribute{
+			Computed:    true,
+			Description: "Regex.",
+		},
+		"prefix_rewrite": schema.StringAttribute{
+			Computed:    true,
+			Description: "Prefix rewrite.",
+		},
+		"regex_rewrite_pattern": schema.StringAttribute{
+			Computed:    true,
+			Description: "Regex rewrite pattern.",
+		},
+		"regex_rewrite_substitution": schema.StringAttribute{
+			Computed:    true,
+			Description: "Regex rewrite substitution.",
+		},
+		"host_rewrite": schema.StringAttribute{
+			Computed:    true,
+			Description: "Host rewrite.",
+		},
+		"host_rewrite_header": schema.StringAttribute{
+			Computed:    true,
+			Description: "Host rewrite header.",
+		},
+		"host_path_regex_rewrite_pattern": schema.StringAttribute{
+			Computed:    true,
+			Description: "Host path regex rewrite pattern.",
+		},
+		"host_path_regex_rewrite_substitution": schema.StringAttribute{
+			Computed:    true,
+			Description: "Host path regex rewrite substitution.",
+		},
+		"regex_priority_order": schema.Int64Attribute{
+			Computed:    true,
+			Description: "Regex priority order.",
+		},
+		"timeout": schema.StringAttribute{
+			Computed:    true,
+			Description: "Timeout.",
+			CustomType:  timetypes.GoDurationType{},
+		},
+		"idle_timeout": schema.StringAttribute{
+			Computed:    true,
+			Description: "Idle timeout.",
+			CustomType:  timetypes.GoDurationType{},
+		},
+		"allow_websockets": schema.BoolAttribute{
+			Computed:    true,
+			Description: "Allow websockets.",
+		},
+		"allow_spdy": schema.BoolAttribute{
+			Computed:    true,
+			Description: "Allow SPDY.",
+		},
+		"tls_skip_verify": schema.BoolAttribute{
+			Computed:    true,
+			Description: "TLS skip verify.",
+		},
+		"tls_upstream_server_name": schema.StringAttribute{
+			Computed:    true,
+			Description: "TLS upstream server name.",
+		},
+		"tls_downstream_server_name": schema.StringAttribute{
+			Computed:    true,
+			Description: "TLS downstream server name.",
+		},
+		"tls_upstream_allow_renegotiation": schema.BoolAttribute{
+			Computed:    true,
+			Description: "TLS upstream allow renegotiation.",
+		},
+		"set_request_headers": schema.MapAttribute{
+			Computed:    true,
+			ElementType: types.StringType,
+			Description: "Set request headers.",
+		},
+		"remove_request_headers": schema.ListAttribute{
+			Computed:    true,
+			ElementType: types.StringType,
+			Description: "Remove request headers.",
+		},
+		"set_response_headers": schema.MapAttribute{
+			Computed:    true,
+			ElementType: types.StringType,
+			Description: "Set response headers.",
+		},
+		"preserve_host_header": schema.BoolAttribute{
+			Computed:    true,
+			Description: "Preserve host header.",
+		},
+		"pass_identity_headers": schema.BoolAttribute{
+			Computed:    true,
+			Description: "Pass identity headers.",
+		},
+		"kubernetes_service_account_token": schema.StringAttribute{
+			Computed:    true,
+			Description: "Kubernetes service account token.",
+		},
+		"idp_client_id": schema.StringAttribute{
+			Computed:    true,
+			Description: "IDP client ID.",
+		},
+		"idp_client_secret": schema.StringAttribute{
+			Computed:    true,
+			Description: "IDP client secret.",
+		},
+		"show_error_details": schema.BoolAttribute{
+			Computed:    true,
+			Description: "Show error details.",
+		},
+	}
+}
+
 func NewRouteDataSource() datasource.DataSource {
 	return &RouteDataSource{}
 }
@@ -30,156 +183,7 @@ func (d *RouteDataSource) Metadata(_ context.Context, req datasource.MetadataReq
 func (d *RouteDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		MarkdownDescription: "Route data source",
-
-		Attributes: map[string]schema.Attribute{
-			"id": schema.StringAttribute{
-				Required:    true,
-				Description: "Unique identifier for the route.",
-			},
-			"name": schema.StringAttribute{
-				Computed:    true,
-				Description: "Name of the route.",
-			},
-			"from": schema.StringAttribute{
-				Computed:    true,
-				Description: "From URL.",
-			},
-			"to": schema.ListAttribute{
-				Computed:    true,
-				ElementType: types.StringType,
-				Description: "To URLs.",
-			},
-			"namespace_id": schema.StringAttribute{
-				Computed:    true,
-				Description: "ID of the namespace the route belongs to.",
-			},
-			"policies": schema.ListAttribute{
-				Computed:    true,
-				ElementType: types.StringType,
-				Description: "List of policy IDs associated with the route.",
-			},
-			"stat_name": schema.StringAttribute{
-				Computed:    true,
-				Description: "Name of the stat.",
-			},
-			"prefix": schema.StringAttribute{
-				Computed:    true,
-				Description: "Prefix.",
-			},
-			"path": schema.StringAttribute{
-				Computed:    true,
-				Description: "Path.",
-			},
-			"regex": schema.StringAttribute{
-				Computed:    true,
-				Description: "Regex.",
-			},
-			"prefix_rewrite": schema.StringAttribute{
-				Computed:    true,
-				Description: "Prefix rewrite.",
-			},
-			"regex_rewrite_pattern": schema.StringAttribute{
-				Computed:    true,
-				Description: "Regex rewrite pattern.",
-			},
-			"regex_rewrite_substitution": schema.StringAttribute{
-				Computed:    true,
-				Description: "Regex rewrite substitution.",
-			},
-			"host_rewrite": schema.StringAttribute{
-				Computed:    true,
-				Description: "Host rewrite.",
-			},
-			"host_rewrite_header": schema.StringAttribute{
-				Computed:    true,
-				Description: "Host rewrite header.",
-			},
-			"host_path_regex_rewrite_pattern": schema.StringAttribute{
-				Computed:    true,
-				Description: "Host path regex rewrite pattern.",
-			},
-			"host_path_regex_rewrite_substitution": schema.StringAttribute{
-				Computed:    true,
-				Description: "Host path regex rewrite substitution.",
-			},
-			"regex_priority_order": schema.Int64Attribute{
-				Computed:    true,
-				Description: "Regex priority order.",
-			},
-			"timeout": schema.StringAttribute{
-				Computed:    true,
-				Description: "Timeout.",
-				CustomType:  timetypes.GoDurationType{},
-			},
-			"idle_timeout": schema.StringAttribute{
-				Computed:    true,
-				Description: "Idle timeout.",
-				CustomType:  timetypes.GoDurationType{},
-			},
-			"allow_websockets": schema.BoolAttribute{
-				Computed:    true,
-				Description: "Allow websockets.",
-			},
-			"allow_spdy": schema.BoolAttribute{
-				Computed:    true,
-				Description: "Allow SPDY.",
-			},
-			"tls_skip_verify": schema.BoolAttribute{
-				Computed:    true,
-				Description: "TLS skip verify.",
-			},
-			"tls_upstream_server_name": schema.StringAttribute{
-				Computed:    true,
-				Description: "TLS upstream server name.",
-			},
-			"tls_downstream_server_name": schema.StringAttribute{
-				Computed:    true,
-				Description: "TLS downstream server name.",
-			},
-			"tls_upstream_allow_renegotiation": schema.BoolAttribute{
-				Computed:    true,
-				Description: "TLS upstream allow renegotiation.",
-			},
-			"set_request_headers": schema.MapAttribute{
-				Computed:    true,
-				ElementType: types.StringType,
-				Description: "Set request headers.",
-			},
-			"remove_request_headers": schema.ListAttribute{
-				Computed:    true,
-				ElementType: types.StringType,
-				Description: "Remove request headers.",
-			},
-			"set_response_headers": schema.MapAttribute{
-				Computed:    true,
-				ElementType: types.StringType,
-				Description: "Set response headers.",
-			},
-			"preserve_host_header": schema.BoolAttribute{
-				Computed:    true,
-				Description: "Preserve host header.",
-			},
-			"pass_identity_headers": schema.BoolAttribute{
-				Computed:    true,
-				Description: "Pass identity headers.",
-			},
-			"kubernetes_service_account_token": schema.StringAttribute{
-				Computed:    true,
-				Description: "Kubernetes service account token.",
-			},
-			"idp_client_id": schema.StringAttribute{
-				Computed:    true,
-				Description: "IDP client ID.",
-			},
-			"idp_client_secret": schema.StringAttribute{
-				Computed:    true,
-				Description: "IDP client secret.",
-			},
-			"show_error_details": schema.BoolAttribute{
-				Computed:    true,
-				Description: "Show error details.",
-			},
-		},
+		Attributes:          getRouteDataSourceAttributes(true),
 	}
 }
 
