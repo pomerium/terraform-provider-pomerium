4dba8e8489706f94f918f38cd4887c32579b4df3
Denis Mishin <dmishin@pomerium.com>
2025-11-24 14:26:34 -0500

    add external_data_source

---
+++ internal/provider/external_data_source_data_source.go
@@ -0,0 +1,135 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	client "github.com/pomerium/enterprise-client-go"
+	"github.com/pomerium/enterprise-client-go/pb"
+)
+
+var (
+	_ datasource.DataSource              = &ExternalDataSourceDataSource{}
+	_ datasource.DataSourceWithConfigure = &ExternalDataSourceDataSource{}
+)
+
+func NewExternalDataSourceDataSource() datasource.DataSource {
+	return &ExternalDataSourceDataSource{}
+}
+
+// ExternalDataSourceDataSource defines the data source implementation.
+type ExternalDataSourceDataSource struct {
+	client *client.Client
+}
+
+// ExternalDataSourceDataSourceModel describes the data source data model.
+type ExternalDataSourceDataSourceModel = ExternalDataSourceModel
+
+func (d *ExternalDataSourceDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_external_data_source"
+}
+
+func (d *ExternalDataSourceDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "External Data Source for Pomerium.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Description: "Unique identifier for the external data source.",
+				Required:    true,
+			},
+			"url": schema.StringAttribute{
+				Description: "The URL to query for data.",
+				Computed:    true,
+			},
+			"record_type": schema.StringAttribute{
+				Description: "How the queried records will be stored in the databroker.",
+				Computed:    true,
+			},
+			"foreign_key": schema.StringAttribute{
+				Description: "The key referenced for policy evaluation. E.g. user.id.",
+				Computed:    true,
+			},
+			"headers": schema.MapAttribute{
+				ElementType: types.StringType,
+				Description: "Request headers sent to the external data source.",
+				Computed:    true,
+			},
+			"allow_insecure_tls": schema.BoolAttribute{
+				Description: "Ignores TLS errors from the external data source.",
+				Computed:    true,
+			},
+			"client_tls_key_id": schema.StringAttribute{
+				Description: "The key pair used for TLS to the external data source.",
+				Computed:    true,
+			},
+			"cluster_id": schema.StringAttribute{
+				Description: "The cluster ID for the external data source.",
+				Computed:    true,
+			},
+			"polling_min_delay": schema.StringAttribute{
+				Description: "The minimum amount of time to wait before polling again.",
+				Computed:    true,
+				CustomType:  timetypes.GoDurationType{},
+			},
+			"polling_max_delay": schema.StringAttribute{
+				Description: "The maximum amount of time to wait before polling again.",
+				Computed:    true,
+				CustomType:  timetypes.GoDurationType{},
+			},
+		},
+	}
+}
+
+func (d *ExternalDataSourceDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	c, ok := req.ProviderData.(*client.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *client.Client, got: %T.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = c
+}
+
+func (d *ExternalDataSourceDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state ExternalDataSourceDataSourceModel
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	respExternalDataSource, err := d.client.ExternalDataSourceService.GetExternalDataSource(ctx, &pb.GetExternalDataSourceRequest{
+		Id: state.ID.ValueString(),
+	})
+	if err != nil {
+		if status.Code(err) == codes.NotFound {
+			resp.Diagnostics.AddError("External Data Source not found", fmt.Sprintf("External Data Source with ID %s not found", state.ID.ValueString()))
+			return
+		}
+		resp.Diagnostics.AddError("get external data source", err.Error())
+		return
+	}
+
+	diags := ConvertExternalDataSourceFromPB(&state, respExternalDataSource.ExternalDataSource)
+	resp.Diagnostics.Append(diags...)
+	if diags.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
