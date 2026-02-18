163c6585522c96fbde903e777613ceb8992c4f72
Caleb Doxsey <caleb@doxsey.net>
2026-02-18 12:26:40 -0700

    wip

---
+++ internal/provider/namespace_resource_test.go
@@ -0,0 +1,41 @@
+package provider_test
+
+import (
+	"encoding/base64"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccNamespace(t *testing.T) {
+	t.Parallel()
+
+	apiURL, sharedSecret := startTestPomeriumCore(t)
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config:      testAccNamespaceConfig(t, apiURL, sharedSecret, "test"),
+				ExpectError: regexp.MustCompile(`unsupported server type`),
+			},
+		},
+	})
+}
+
+func testAccNamespaceConfig(t *testing.T, apiURL string, sharedSecret []byte, name string) string {
+	t.Helper()
+
+	return fmt.Sprintf(`
+provider "pomerium" {
+  api_url           = "%s"
+  shared_secret_b64 = "%s"
+}
+
+resource "pomerium_namespace" "test" {
+	name = "%s"
+}	
+`, apiURL, base64.StdEncoding.EncodeToString(sharedSecret), name)
+}
