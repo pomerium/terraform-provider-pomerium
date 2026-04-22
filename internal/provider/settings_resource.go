29fbb006e67f39113265688fb8dace7ea63f5fd0
Caleb Doxsey <cdoxsey@pomerium.com>
2026-04-22 15:15:57 -0600

    terraform/settings: set zero defaults (#222)
    Currently if you apply settings to zero, many settings will be erased
    because zero doesn't layer settings the same way core and enterprise
    does. It defines the entirety of the configuration and sets many fields
    to defaults (like `address` as `:443`).
    
    This PR adds a new `ModifyPlan` method on the settings resource that
    will set unknown fields to these default values, but just for zero. For
    the other products they will be set to null.
    
    This PR depends on #220

--- internal/provider/settings_resource.go
+++ internal/provider/settings_resource.go
@@ -2,10 +2,14 @@ package provider
 
 import (
 	"context"
+	"time"
 
 	"connectrpc.com/connect"
+	"github.com/hashicorp/terraform-plugin-framework-timetypes/timetypes"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	client "github.com/pomerium/enterprise-client-go"
 	"github.com/pomerium/enterprise-client-go/pb"
@@ -16,6 +20,7 @@ import (
 var _ interface {
 	resource.Resource
 	resource.ResourceWithImportState
+	resource.ResourceWithModifyPlan
 } = (*SettingsResource)(nil)
 
 func NewSettingsResource() resource.Resource {
@@ -227,4 +232,147 @@ func (r *SettingsResource) Delete(ctx context.Context, req resource.DeleteReques
 
 func (r *SettingsResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
 	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
+
+func (r *SettingsResource) ModifyPlan(ctx context.Context, req resource.ModifyPlanRequest, resp *resource.ModifyPlanResponse) {
+	// this is a deletion, nothing to do
+	if req.Plan.Raw.IsNull() {
+		return
+	}
+
+	var model SettingsModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &model)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(r.client.ByServerType(
+		func(_ sdk.CoreClient) {
+			setSettingsDefaults(&model, false)
+		},
+		func(_ *client.Client) {
+			setSettingsDefaults(&model, false)
+		},
+		func(_ sdk.ZeroClient) {
+			setSettingsDefaults(&model, true)
+		})...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	resp.Diagnostics.Append(resp.Plan.Set(ctx, model)...)
+}
+
+func setSettingsDefaults(model *SettingsModel, isZero bool) {
+	if isZero {
+		if model.Address.IsUnknown() {
+			model.Address = types.StringValue(":443")
+		}
+		if model.AutoApplyChangesets.IsUnknown() {
+			model.AutoApplyChangesets = types.BoolValue(true)
+		}
+		if model.Autocert.IsUnknown() {
+			model.Autocert = types.BoolValue(false)
+		}
+		if model.CookieExpire.IsUnknown() {
+			model.CookieExpire = timetypes.NewGoDurationValue(14 * time.Hour)
+		}
+		if model.CookieHTTPOnly.IsUnknown() {
+			model.CookieHTTPOnly = types.BoolValue(true)
+		}
+		if model.CookieName.IsUnknown() {
+			model.CookieName = types.StringValue("_pomerium")
+		}
+		if model.DefaultUpstreamTimeout.IsUnknown() {
+			model.DefaultUpstreamTimeout = timetypes.NewGoDurationValue(30 * time.Second)
+		}
+		if model.DNSLookupFamily.IsUnknown() {
+			model.DNSLookupFamily = types.StringValue("V4_PREFERRED")
+		}
+		if model.GRPCAddress.IsUnknown() {
+			model.GRPCAddress = types.StringValue(":443")
+		}
+		if model.GRPCInsecure.IsUnknown() {
+			model.GRPCInsecure = types.BoolValue(false)
+		}
+		if model.InsecureServer.IsUnknown() {
+			model.InsecureServer = types.BoolValue(false)
+		}
+		if model.LogLevel.IsUnknown() {
+			model.LogLevel = types.StringValue("info")
+		}
+		if model.MCPAllowedClientIDDomains.IsUnknown() {
+			model.MCPAllowedClientIDDomains = types.SetValueMust(types.StringType, []attr.Value{types.StringValue("vscode.dev")})
+		}
+		if model.PassIdentityHeaders.IsUnknown() {
+			model.PassIdentityHeaders = types.BoolValue(false)
+		}
+		if model.SkipXFFAppend.IsUnknown() {
+			model.SkipXFFAppend = types.BoolNull()
+		}
+		if model.TimeoutIdle.IsUnknown() {
+			model.TimeoutIdle = timetypes.NewGoDurationValue(5 * time.Minute)
+		}
+		if model.TimeoutRead.IsUnknown() {
+			model.TimeoutRead = timetypes.NewGoDurationValue(30 * time.Second)
+		}
+		if model.TimeoutWrite.IsUnknown() {
+			model.TimeoutWrite = timetypes.NewGoDurationNull()
+		}
+	} else {
+		if model.Address.IsUnknown() {
+			model.Address = types.StringNull()
+		}
+		if model.AutoApplyChangesets.IsUnknown() {
+			model.AutoApplyChangesets = types.BoolNull()
+		}
+		if model.Autocert.IsUnknown() {
+			model.Autocert = types.BoolValue(false)
+		}
+		if model.CookieExpire.IsUnknown() {
+			model.CookieExpire = timetypes.NewGoDurationNull()
+		}
+		if model.CookieHTTPOnly.IsUnknown() {
+			model.CookieHTTPOnly = types.BoolNull()
+		}
+		if model.CookieName.IsUnknown() {
+			model.CookieName = types.StringNull()
+		}
+		if model.DefaultUpstreamTimeout.IsUnknown() {
+			model.DefaultUpstreamTimeout = timetypes.NewGoDurationNull()
+		}
+		if model.DNSLookupFamily.IsUnknown() {
+			model.DNSLookupFamily = types.StringNull()
+		}
+		if model.GRPCAddress.IsUnknown() {
+			model.GRPCAddress = types.StringNull()
+		}
+		if model.GRPCInsecure.IsUnknown() {
+			model.GRPCInsecure = types.BoolNull()
+		}
+		if model.InsecureServer.IsUnknown() {
+			model.InsecureServer = types.BoolNull()
+		}
+		if model.LogLevel.IsUnknown() {
+			model.LogLevel = types.StringNull()
+		}
+		if model.MCPAllowedClientIDDomains.IsUnknown() {
+			model.MCPAllowedClientIDDomains = types.SetNull(types.StringType)
+		}
+		if model.PassIdentityHeaders.IsUnknown() {
+			model.PassIdentityHeaders = types.BoolNull()
+		}
+		if model.SkipXFFAppend.IsUnknown() {
+			model.SkipXFFAppend = types.BoolNull()
+		}
+		if model.TimeoutIdle.IsUnknown() {
+			model.TimeoutIdle = timetypes.NewGoDurationNull()
+		}
+		if model.TimeoutRead.IsUnknown() {
+			model.TimeoutRead = timetypes.NewGoDurationNull()
+		}
+		if model.TimeoutWrite.IsUnknown() {
+			model.TimeoutWrite = timetypes.NewGoDurationNull()
+		}
+	}
 }
